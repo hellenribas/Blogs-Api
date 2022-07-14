ced4a9527efeb0f9c1ade88aba2d065b331adcc1
hellenribas <tchwtrabalho@gmail.com>
2022-07-14 16:49:25 -0300

    feat(model): created user

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const createUserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+  })
+  return User;
+};
+
+module.exports = createUserModel
